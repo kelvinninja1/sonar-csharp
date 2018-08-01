2426243e3683c6369ade9e26dd419b74fce54229
duncanp <duncan.pocklington@sonarsource.com>
2018-08-01 18:33:55 +0100

    Added skeleton projects for security analyzer

---
+++ sonaranalyzer-dotnet/src/SecurityProofOfConcept/SonarSecurity.Analyzer.Test/SonarSecurityAnalyzerUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestHelper;
+using SonarSecurity.Analyzer;
+
+namespace SonarSecurity.Analyzer.Test
+{
+    [TestClass]
+    public class UnitTest : DiagnosticVerifier
+    {
+
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = @"";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new SonarSecurityAnalyzer();
+        }
+    }
+}
