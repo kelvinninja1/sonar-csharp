341e86018fefa9ed77b82793627c21ffa9967836
Michael Gumowski <gumowski.michael@gmail.com>
2018-08-15 15:35:47 +0200

    Move every Security-related class and copy CFG

---
+++ sonaranalyzer-dotnet/src/SecurityProofOfConcept/SonarSecurity.Analyzer/Protobuf/build.bat
@@ -0,0 +1,1 @@
+"..\..\..\..\packages\Google.Protobuf.Tools.3.4.0\tools\windows_x64\protoc.exe" -I=./../../../../../sonar-ucfg/src/main/protobuf/ --csharp_out=. ./../../../../../sonar-ucfg/src/main/protobuf/ucfg.proto
